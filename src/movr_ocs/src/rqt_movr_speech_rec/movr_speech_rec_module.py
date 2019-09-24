8d19bc76a99e6f9210734de94fb0b76897545247
MaidenoftheMain <Marcus.Arnett10@gmail.com>
2019-09-24 22:18:09 +1000

    Add additional functionality to clear cache and improving storage

--- src/movr_ocs/src/rqt_movr_speech_rec/movr_speech_rec_module.py
+++ src/movr_ocs/src/rqt_movr_speech_rec/movr_speech_rec_module.py
@@ -18,7 +18,6 @@ import sys
 from gtts import gTTS
 from playsound import playsound
 import speech_recognition as sr
-import time
 from os import path
 import keyboard
 
@@ -29,7 +28,6 @@ assert numpy
 
 # https://medium.com/@webmamoffice/getting-started-gui-s-with-python-pyqt-qthread-class-1b796203c18c
 class SpeechRecognitionThread(QThread):
-    # rec_done_sig = Signal([bool, str])
     rec_done_sig = Signal([str])
     sr_done_sig = Signal([str])
     def __init__(self, parent=None):
@@ -42,43 +40,37 @@ class SpeechRecognitionThread(QThread):
         self._is_rec_btn_pressed = state
 
     def callback(self, indata, frames, time, status):
-        # print(status, file=sys.stderr)
-        # if status:
-            # print(status, file=sys.stderr)
         self.q.put(indata.copy())
 
     def run(self):
-        # text_transcribed_text
         self.running = True
-        # while self.running:
-        #    time.sleep(1)
+        path = "/home/robocup2019/movr/movr_ws/src/movr_ocs/src/rqt_movr_speech_rec/"
         device_info = sd.query_devices(None, 'input')
-        filename = tempfile.mktemp(prefix='Human_',suffix='.wav', dir='/home/robocup2019/movr/movr_ws/src/movr_ocs/src/rqt_movr_speech_rec')
+        filename = tempfile.mktemp(prefix='Human_', suffix='.wav', dir= path + 'human_files')
         samplerate = int(device_info['default_samplerate'])
 
-        with sf.SoundFile(filename, mode='x', samplerate=samplerate,
-                    channels=1, subtype="PCM_24") as file:
-            with sd.InputStream(samplerate=samplerate, device=None,
-                                channels=1, callback=self.callback):       
+        with sf.SoundFile(filename, mode='x', samplerate=samplerate,channels=1, subtype="PCM_24") as file:
+            with sd.InputStream(samplerate=samplerate, device=None,channels=1, callback=self.callback):       
                 while self.running:
                     file.write(self.q.get())            
-        # self.rec_done_sig.emit("DONE RECORDING...")
         self.rec_done_sig.emit("DONE RECORDING...")
-        print('\nRecording finished: ' + repr(filename))
-        AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), filename)
+        
+        self.rec_done_sig.emit("Recording finished: " + repr(filename))
         r = sr.Recognizer()
-        with sr.AudioFile(AUDIO_FILE) as source:
+        with sr.AudioFile(filename) as source:
             audio = r.record(source)
-        text = r.recognize_google(audio)
-        self.sr_done_sig.emit(text)
-        tts = gTTS(text, 'en')
-        text = text.replace(" ", "_")
-        robotFile = text + '.mp3'
-        if not os.path.exists('/home/robocup2019/movr/movr_ws/src/movr_ocs/src/rqt_movr_speech_rec' + robotFile):
-            tts.save('/home/robocup2019/movr/movr_ws/src/movr_ocs/src/rqt_movr_speech_rec' + robotFile)
+        transcribed_text = r.recognize_google(audio)
+        self.sr_done_sig.emit(transcribed_text)
+        tts = gTTS(transcribed_text, 'en')
+        transcribed_text = transcribed_text.replace(" ", "_")
+        robotFile = transcribed_text + '.mp3'
+        if not os.path.exists(path + 'robot_files/' + robotFile):
+            self.rec_done_sig.emit("SAVING NEW RECORDING...")
+            tts.save(path + 'robot_files/' + robotFile)
             if robotFile not in self.robo:
                 self.robo[robotFile] = robotFile
-        playsound('/home/robocup2019/movr/movr_ws/src/movr_ocs/src/rqt_movr_speech_rec' + robotFile)
+        self.rec_done_sig.emit("PLAYING TTS FILE...")
+        playsound(path + 'robot_files/' + robotFile)
         robotFile = ''
         self.rec_done_sig.emit("DONE RECORDING...")
 
@@ -94,65 +86,64 @@ class MOVRSpeechRecPlugin(Plugin):
         from argparse import ArgumentParser
         parser = ArgumentParser()
         # Add argument(s) to the parser.
-        parser.add_argument("-q", "--quiet", action="store_true",
-                      dest="quiet",
-                      help="Put plugin in silent mode")
+        parser.add_argument("-q", "--quiet", action="store_true",dest="quiet",help="Put plugin in silent mode")
         args, unknowns = parser.parse_known_args(context.argv())
         if not args.quiet:
             print 'arguments: ', args
             print 'unknowns: ', unknowns
 
-        # Create QWidget
-        self._widget = QWidget()
-        # Get path to UI file which should be in the "resource" folder of this package
+        self._widget = QWidget() # Create QWidget
         ui_file = os.path.join(rospkg.RosPack().get_path('movr_ocs'), 'resource', 'MOVRSpeechRecPlugin.ui')
-        # Extend the widget with all attributes and children from UI file
-        loadUi(ui_file, self._widget)
-        # Give QObjects reasonable names
-        self._widget.setObjectName('MOVRSpeechRecPlugin')
+        # Get path to UI file which should be in the "resource" folder of this package
 
+        loadUi(ui_file, self._widget)
+        # Extend the widget with all attributes and children from UI file
+
+        self._widget.setObjectName('MOVRSpeechRecPlugin')
+        # Give QObjects reasonable names
 
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
-        # Add widget to the user interface
-        context.add_widget(self._widget)
 
+        context.add_widget(self._widget)
+        # Add widget to the user interface
 
         self.btn_clicked = False
-        # Add Event Handler
+        
         # self.btn_rec_state_changed_sig.connect(self.on_btn_state_changed)
-        self._widget.btn_record.clicked.connect(self.on_btn_rec_clicked)
 
-        self._white_string  = "background-color: rgb(255,255,255); color: #000;"
+        self._widget.btn_clear.clicked.connect(self.clear_cache)
+
+        self._widget.btn_record.clicked.connect(self.on_btn_rec_clicked)
+        # Add Event Handler
+
         self._red_string    = "background-color: rgb(255,0,0); color: #FFF;"
-        self._orange_string = "background-color: rgb(255,128,0)"
-        self._yellow_string = "background-color: rgb(255,255,0)"
         self._green_string  = "background-color: rgb(128,255,0)"
-        self._black_string  = "background-color: rgb(0,0,0)"
-        self._grey_string   = "background-color: rgb(160,160,160); color: #606060"
 
     def on_btn_rec_clicked(self):
 
         if not self.btn_clicked:
             self.btn_clicked = True
-            # Launch SpeechRecognition Thread
-            self.rec_thread = SpeechRecognitionThread()
+            self.rec_thread = SpeechRecognitionThread() # Launch SpeechRecognition Thread
+            self.btn_rec_state_changed_sig.connect(self.rec_thread.on_rec_btn_press) 
             # Connect (this) signal to a function in the thread
-            self.btn_rec_state_changed_sig.connect(self.rec_thread.on_rec_btn_press)
-            # Tell the thread that we have clicked something (set the state)
-            self.btn_rec_state_changed_sig.emit(self.btn_clicked)
-            # Start thread
-            self.rec_thread.start()
 
+            self.btn_rec_state_changed_sig.emit(self.btn_clicked)
+            # Tell the thread that we have clicked something (set the state)
+ 
+            self.rec_thread.start() # Start thread
+
+            self.rec_thread.rec_done_sig.connect(self.update_rec_info) 
             # Connect thread's signal to a function in (this) class
-            self.rec_thread.rec_done_sig.connect(self.update_rec_info)
-            # Change button's color
-            self._widget.btn_record.setStyleSheet(self._green_string)
+
+            self._widget.btn_record.setStyleSheet(self._green_string) # Change button's color
             self._widget.text_logs.setPlainText("RECORDING")
 
         else:
             self.rec_thread.running = False
-            # self.but1.setEnabled(True)           
+
+            # self.but1.setEnabled(True)    
+       
             self._widget.btn_record.setStyleSheet(self._red_string)
             self.btn_clicked = False     
             self._widget.text_logs.setPlainText("STOPPING RECORDING...")
@@ -161,18 +152,21 @@ class MOVRSpeechRecPlugin(Plugin):
             # self.btn_rec_state_changed.emit(self.btn_clicked)
             # self.btn_clicked = not self.btn_clicked
 
-    def update_rec_info(self, str):
-        '''
-        Function used by thread when it is done runnning
-        '''
+    def update_rec_info(self, str): # Function used by thread when it is done runnning
         self._widget.text_logs.setPlainText(str)
         rospy.loginfo(str)
         rospy.loginfo("DONE!!!")
     
-    def update_sr_output(self, str):
+    def update_sr_output(self, str): # Function used by thread to update output display
         self._widget.text_transcribed_text.setPlainText(str)
         rospy.loginfo(str)
         rospy.loginfo("Updated SR Output.")
+
+    def clear_cache(self): # Function used to clear robot voice cache
+        self._widget.text_logs.setPlainText("Clearing Robot and Human Cache")
+        os.system("rm -rf /home/robocup2019/movr/movr_ws/src/movr_ocs/src/rqt_movr_speech_rec/robot_files/*.mp3")
+        os.system("rm -rf /home/robocup2019/movr/movr_ws/src/movr_ocs/src/rqt_movr_speech_rec/human_files/*.wav")
+        rospy.loginfo("Cleared Robot and Human Cache.")
 
     # def update_button_state(self, state):
     #     if state == True:
