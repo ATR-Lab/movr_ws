3f430cce40c3df081c72f9bf8034ae445cc77144
kPatch <irvsteve@gmail.com>
2019-09-29 01:07:35 -0400

    Add rf testing

--- src/movr_teleop/src/test_rf.py
+++ src/movr_teleop/src/test_rf.py
@@ -9,8 +9,10 @@ while True:
     # ser.write("<c 120 100>".encode())
     # ser.write(b'hello')
     # ser.write(65)
-    ser.write('<c 100, 120>'.encode('utf-8'))
-    ser.write('<m Do you need a ride?>'.encode('utf-8'))
+    # ser.write('<c 100, 120>'.encode('utf-8'))
+    # ser.write('<m Do you need a ride?>'.encode('utf-8'))
+    ser.write(b'<c 100,120>')
+    # ser.write(b'<m Do you need a ride?>')
     time.sleep(1)
 
 ser.close()
