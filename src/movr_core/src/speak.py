c00c555dcb0646b66ec0e358a28d2f6cda768436
kPatch <irvsteve@gmail.com>
2019-10-01 14:36:34 -0400

    Add initial version of speak node

--- src/movr_core/src/speak.py
+++ src/movr_core/src/speak.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import sys
 import rospy
@@ -5,6 +6,7 @@ import rospkg
 from std_msgs.msg import String
 from gtts import gTTS
 from playsound import playsound
+from tinydb import TinyDB, Query
 
 # get an instance of RosPack with the default search paths
 rospack = rospkg.RosPack()
@@ -13,12 +15,15 @@ movr_ocs_pkg = rospack.get_path('movr_core')
 
 robo = {}
 
-def op_msg_cb(msg):
-    text = msg.data
+db = TinyDB(movr_ocs_pkg + '/db/db.json')
 
-    tts = gTTS(text, 'en')
-    text = text.replace(" ", "_")
-    robotFile = text + '.mp3'
+
+def op_msg_cb(msg):
+    text = msg.data         # Get text from ros message
+    tts = gTTS(text, 'en')  # Process text to speech
+
+    text = text.replace(" ", "_")   # Replace space with '-'
+    robotFile = text + '.mp3'       # The sound file name
 
     robot_files_path = movr_ocs_pkg + '/recordings/robot_files/'
 
@@ -30,16 +35,16 @@ def op_msg_cb(msg):
         tts.save(robot_files_path + robotFile)
         if robotFile not in robo:
             robo[robotFile] = robotFile
-    playsound(robot_files_path + robotFile)
-    robotFile = ''
+
+    playsound(robot_files_path + robotFile) # Play sound
 
 if __name__ == "__main__":
 	
-	args = rospy.myargv(argv=sys.argv)
-	if len(args) != 2:
-		port = '/dev/ttyUSB0'
-	else:
-		port = args[1]
+	# args = rospy.myargv(argv=sys.argv)
+	# if len(args) != 2:
+	# 	port = '/dev/ttyUSB0'
+	# else:
+	# 	port = args[1]
 
 	rospy.init_node('movr_core_speak')
 	sub = rospy.Subscriber("movr_op_msg", String, op_msg_cb)
